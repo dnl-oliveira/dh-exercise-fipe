6381ba4c47b406eac1ebba0df55167cacd9cb67a
Daniel Oliveira Nascimento <contato@danieloliveira.eti.br>
2020-11-30 10:16:50 -0300

    initial commit

---
+++ 20201130-MVVM-Fipe/Pods/Target Support Files/Pods-20201130-MVVM-Fipe/Pods-20201130-MVVM-Fipe-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_20201130_MVVM_Fipe : NSObject
+@end
+@implementation PodsDummy_Pods_20201130_MVVM_Fipe
+@end
---
+++ 20201130-MVVM-Fipe/Pods/Target Support Files/Pods-20201130-MVVM-FipeTests/Pods-20201130-MVVM-FipeTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_20201130_MVVM_FipeTests : NSObject
+@end
+@implementation PodsDummy_Pods_20201130_MVVM_FipeTests
+@end
